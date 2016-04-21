c73f1199f382d3395ef4e0ac2f53436970a6d180
Angel Lo <angello@AngeldeMacBook-Pro-2.local>
2016-04-21 20:35:42 +0800

    modified

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,19 +1,21 @@
-
-
-function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
-}
-function randomPlay() {
-    var randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-        return "rock";
-    } else if (randomNumber < 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
+ /*   Provided Code - Please Don't Edit   */
+ ////////////////////////////////////////////////
+ 'use strict';
+ 
+ function getInput() {
+     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+     return prompt();
+ }
+ function randomPlay() {
+     var randomNumber = Math.random();
+     if (randomNumber < 0.33) {
+         return "rock";
+     } else if (randomNumber < 0.66) {
+         return "paper";
+     } else {
+         return "scissors";
+     }
+ }
 ////////////////////////////////////////////////
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
@@ -32,30 +34,28 @@ function getComputerMove() {
 
 function getWinner(playerMove,computerMove) {
    var winner;
-    
     if (playerMove===computerMove){
     	winner="tie";
-        return "TIE!"}
-        
+        }
     else if (computerMove==="rock"){
         if (playerMove==="scissors"){
             winner="computer";
-            return "COMPUTER WINS";
+           
         }
           else {
           	winner="player";
-              return "PLAYER WINS" ;
+             
           }
      }
     
      else if (computerMove==="scissors"){
      	if (playerMove==="paper"){
      		winner="computer";
-            return "COMPUTER WINS";
+            
         }
           else {
              winner="player";
-             return "PLAYER WINS";
+          
              
           }
         }
@@ -63,42 +63,40 @@ function getWinner(playerMove,computerMove) {
      else if (computerMove==="paper"){
      	if (playerMove==="rock"){
      		winner="computer";
-            return"COMPUTER WINS";
+            
      	}
           else
              {
                 winner="player";
-             	return "PLAYER WINS" ;
+             	
               }
         }
    return winner;
   
 }
 
-function playtoFive() {
+function playtoFive(winner) {
   console.log('Let\'s play Rock Paper Scissors');
   var playerWins = 0;
   var computerWins = 0;
-  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
-  console.log(getWinner(playerMove,computerMove));
-    if ((getWinner())==="player"){
-    	playerWins= playerWins+1;
-    	console.log(playerWins);
-    }
-    else if (getWinner()==="computer"){
-    	computerWins =computerWins+1;
-    	console.log(computerWins);
-    }
+  while (playerWins < 5 && computerWins < 5){
     
-    else 	{
-    	computerWins =computerWins+0;
-    	playerWins= playerWins+0;
+  console.log(winner +' wins!');
+    if (winner==="player"){
+    	playerWins+=1;
+   
     }
-    
-    console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
-    
+    else if (winner==="computer"){
+    	computerWins +=1;
+    }
+    return [playerWins, computerWins];
 }
+}
+
 var playerMove=getPlayerMove();
 var computerMove=getComputerMove();
-
+console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+var winner=getWinner(playerMove,computerMove);
+var [playerWins,computerWins]= playtoFive(winner);
+console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
     
